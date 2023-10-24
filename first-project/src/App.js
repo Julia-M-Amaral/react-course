61b557bb30397c428b5dd4531640e395a8f08f8b
Julia Mangabeira <JuliaAmaralSantos@outlook.com>
2023-10-24 03:47:09 -0300

    utilizando props para inserir dados de fora

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -2,11 +2,55 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
 
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 10, 24)
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 899.02,
+      date: new Date(2023, 10, 24)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2023, 10, 24)
+    },
+    {
+      id: 'e4',
+      title: 'New Desk',
+      amount: 2367.1,
+      date: new Date(2023, 10, 24)
+    }
+
+  ];
+
   return (
-   <div>
-    <h2>hello!</h2>
-    <ExpenseItem />
-   </div>
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      /><ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      /><ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </div>
   );
 }
 
