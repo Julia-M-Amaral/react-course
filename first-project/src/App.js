e62c68e4a6b20ba7650afeaa07db758d76823490
Julia Mangabeira <JuliaAmaralSantos@outlook.com>
2023-10-27 18:16:14 -0300

    criacao do componente Expenses que é o display do array de objetos expenses do app.js

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
+
 
 function App() {
 
@@ -33,21 +34,8 @@ function App() {
   return (
     
     <div>
-      <ExpenseItem
-        expense={expenses[0]}
-      />
       
-      <ExpenseItem
-        expense={expenses[1]}
-      />
-
-      <ExpenseItem
-        expense={expenses[2]}
-      />
-
-      <ExpenseItem
-        expense={expenses[3]}
-      />
+     <Expenses items={expenses}/>
 
     </div>
   );
