fd89bd4424efc0398c924a7f4b2d9d3e444d845d
Julia Mangabeira <JuliaAmaralSantos@outlook.com>
2023-10-27 17:12:24 -0300

    adicionando novo componente ExpenseDate

--- first-project/src/components/ExpenseItem.js
+++ first-project/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
+
   return (
     <div className="expense-item">
-      <div>{props.expense.date.toISOString()}</div>
+      <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
         <h2>{props.expense.title}</h2>
         <div className="expense-item__price"> ${props.expense.amount} </div>
